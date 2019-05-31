407a43f1d2db7833441475b7b7cb881f1ca2b22b
phc5403 <phc5403@gmail.com>
2019-05-31 13:07:46 +0900

    static&media exer

--- secondproject/urls.py
+++ secondproject/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 import blog.views
+import portfolio.views
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,8 +12,9 @@ urlpatterns = [
     path('blog/<int:blog_id>/', blog.views.detail, name='detail'),
     path('blog/new/', blog.views.new, name='new'),
     path('blog/create/', blog.views.create, name='create'),
+    path('portfolio/', portfolio.views.portfolio, name='portfolio'),
     
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
 
