3cea274144041d06ee08f036aae074bd0b2d9e08
phc5403 <phc5403@gmail.com>
2019-08-26 17:57:28 +0900

    BackUp-8.26

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blog
 from django.utils import timezone
+from django.core.paginator import Paginator
 
 def home(request):
     blogs = Blog.objects
-    return render(request, 'blog/home.html', {'blogs' : blogs})
+    blog_list = Blog.objects.all()
+    paginator = Paginator(blog_list,4)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'blog/home.html', {'blogs' : blogs, 'posts':posts})
     
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
